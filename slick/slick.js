1d2a51e31ad5960cf35ef148f9a415f72670295d
Sander Heling <sanderheling@gmail.com>
2015-12-15 16:57:39 +0100

    Fix to make slick work with browerify
    Also see: https://github.com/kenwheeler/slick/issues/1010

--- slick/slick.js
+++ slick/slick.js
@@ -15,17 +15,7 @@
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
-(function(factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        define(['jquery'], factory);
-    } else if (typeof exports !== 'undefined') {
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(jQuery);
-    }
-
-}(function($) {
+;(function ( $, window, document, undefined ) {
     'use strict';
     var Slick = window.Slick || {};
 
@@ -2667,4 +2657,4 @@
         return _;
     };
 
-}));
+})( jQuery, window, document );
